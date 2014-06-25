44a903589a93cbaf3b59a681c9aa4e1a71d49c0b
Sonny Scroggin <scrogson@gmail.com>
2014-06-24 20:07:41 -0500

    Initial commit

---
+++ hello-world/scrogson+plug/README.md
@@ -0,0 +1,13 @@
+HelloWorld
+==========
+
+This is a Hello World example that uses
+[Plug](https://github.com/elixir-lang/plug) in
+[Elixir](https://github.com/elixir-lang/elixir)
+
+## Usage
+
+```bash
+mix deps.get
+mix run --no-halt hello_world.exs
+```
---
+++ hello-world/scrogson+plug/mix.exs
@@ -0,0 +1,35 @@
+defmodule HelloWorld.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :hello_world,
+     version: "0.0.1",
+     elixir: "~> 0.14.1",
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [
+      applications: [:cowboy, :plug],
+    ]
+  end
+
+  # Dependencies can be hex.pm packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    [
+      {:cowboy, github: "extend/cowboy"},
+      {:plug, "~> 0.4.2"}
+    ]
+  end
+end
